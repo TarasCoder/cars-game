c6314ea23674004fe088cb3735b10d3e7c361381
kaskad <kaskad@bigmir.net>
2021-06-29 00:32:07 +0300

    add images and basic js functions

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
 const score = document.querySelector(".score");
 const start = document.querySelector(".start");
 const gameArea = document.querySelector(".gameArea");
-
-start.addEventListener('click',startGame)
+const car = document.createElement("div")
+car.classList.add('car')
+const keys = {
+  ArrowUp: false,
+  ArrowDown: false,
+  ArrowRight: false,
+  ArrowLeft: false,
+};
+const setting = {
+    start: false,
+    score: 0,
+    speed: 3
+}
+start.addEventListener("click", startGame);
 document.addEventListener("keydown", startRun);
 document.addEventListener("keyup", stopRun);
 
-function startGame(){
-start.classList.add("hide");
+function startGame() {
+  start.classList.add("hide");
+  setting.start = true
+  gameArea.appendChild(car)
+  requestAnimationFrame(playGame);
 }
+function playGame() {
+  console.log("play game");
+  if(setting.start === true){
+      requestAnimationFrame(playGame);
 
-function startRun() {
-  console.log("start");
+  }
 }
-function stopRun() {
-  console.log("stop");
+function startRun(event) {
+  event.preventDefault();
+  keys[event.key] = true;
+}
+function stopRun(event) {
+  event.preventDefault();
+  keys[event.key] = false;
 }
