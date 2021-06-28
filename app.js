1101de9da29fb7c310ed6afeb6bd46d8fd773b0a
kaskad <kaskad@bigmir.net>
2021-06-29 00:42:02 +0300

    fix f5 button issue

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const score = document.querySelector(".score");
 const start = document.querySelector(".start");
 const gameArea = document.querySelector(".gameArea");
-const car = document.createElement("div")
-car.classList.add('car')
+const car = document.createElement("div");
+car.classList.add("car");
 const keys = {
   ArrowUp: false,
   ArrowDown: false,
@@ -10,30 +10,33 @@ const keys = {
   ArrowLeft: false,
 };
 const setting = {
-    start: false,
-    score: 0,
-    speed: 3
-}
+  start: false,
+  score: 0,
+  speed: 3,
+};
 start.addEventListener("click", startGame);
 document.addEventListener("keydown", startRun);
 document.addEventListener("keyup", stopRun);
 
 function startGame() {
   start.classList.add("hide");
-  setting.start = true
-  gameArea.appendChild(car)
+  setting.start = true;
+  gameArea.appendChild(car);
   requestAnimationFrame(playGame);
 }
 function playGame() {
   console.log("play game");
-  if(setting.start === true){
-      requestAnimationFrame(playGame);
-
+  if (setting.start === true) {
+    requestAnimationFrame(playGame);
   }
 }
 function startRun(event) {
-  event.preventDefault();
-  keys[event.key] = true;
+  if (event.key === "F5") {
+    keys[event.key] = true;
+  } else {
+    event.preventDefault();
+    keys[event.key] = true;
+  }
 }
 function stopRun(event) {
   event.preventDefault();
